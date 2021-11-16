28d7b11429682671b4c88f2df2ac4b249d7e50cf
LD <chiyc88@gmail.com>
2021-11-16 22:53:06 +0800

    Initial commit

---
+++ app/src/main/java/com/gmail/chiyc88/busymesimulator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.chiyc88.busymesimulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
